b3fbbe4b625b6c04fb948c1bf7b4990f2eb65ac2
abbaadamulele001-maker <abbaadamulele001@gmail.com>
2026-03-07 13:13:50 +0100

    Create scripts/java_script.java

---
+++ scripts/java_script.java
@@ -0,0 +1,8 @@
+public class java_script {
+    public static void main(String[] args) {
+        System.out.println("Abba Adamu");
+        System.out.println("abbaadamu@email.com");
+        System.out.println("@abbaadamu");
+        System.out.println("Genomics");
+    }
+}
