5fc98a952e65289061111220cb1fd56680a6a326
abbaadamulele001-maker <abbaadamulele001@gmail.com>
2026-03-07 13:17:06 +0100

    Create scripts/cpp_script.cpp

---
+++ scripts/cpp_script.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Abba Adamu" << endl;
+    cout << "abbaadamu@email.com" << endl;
+    cout << "@abbaadamu" << endl;
+    cout << "Genomics" << endl;
+    return 0;
+}
