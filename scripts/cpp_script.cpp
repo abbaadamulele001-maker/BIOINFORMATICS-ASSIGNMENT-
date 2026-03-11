6d7fd25792104aac29f8425b0e54471330cc5fab
abbaadamulele001-maker <abbaadamulele001@gmail.com>
2026-03-11 13:21:17 +0100

    Create cpp_script.cpp

---
+++ scripts/cpp_script.cpp
@@ -0,0 +1,10 @@
+#include using namespace std;
+
+int main() {
+
+cout << "Raufa Ahmad Dashe" << endl;
+cout << "ahmadraufa695@gmaill.com" << endl;
+cout << "@raufa" << endl;
+cout << "Gene ontology" << endl;
+
+return 0;
