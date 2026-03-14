f20720a1a73379a3828f72fed24a1b51cbae6630
abbaadamulele001-maker <abbaadamulele001@gmail.com>
2026-03-14 07:20:26 +0100

    Update python_script.py

--- scripts/python_script.py
+++ scripts/python_script.py
@@ -1,4 +1,14 @@
-print("Abba Adamu")
-print("abbaadamu@email.com")
-print("@abbaadamu")
-print("Genomics")
+# Bioinformatics Assignment Script
+# Author: Abba ADAMU Lele
+
+# Define personal information
+full_name = "Abba ADAMU Lele"
+email = "abbaadamulele001@gmail.com"
+slack_username = "@Abba Adamu"
+area_of_interest = "Genomics"
+
+# Print the information in the required order
+print("I. Full Name:", full_name)
+print("II. Email Address:", email)
+print("III. Slack Username:", slack_username)
+print("IV. Area of Interest in Bioinformatics:", area_of_interest)
